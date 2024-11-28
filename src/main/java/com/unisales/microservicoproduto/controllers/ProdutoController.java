acc869a314bdccb43d1a2e00d660f07a2ecfe823
lucasguimaraes2005 <lucaagui123@gmail.com>
2024-11-28 17:03:47 -0300

    aa

--- src/main/java/com/unisales/microservicoproduto/controllers/ProdutoController.java
+++ src/main/java/com/unisales/microservicoproduto/controllers/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.unisales.microservicoproduto.services.ProdutoService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "https://angular-microservicos.vercel.app")
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
