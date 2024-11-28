c853e35ef34c2d5d0eecd4538de617cd20105517
lucasguimaraes2005 <lucaagui123@gmail.com>
2024-11-28 16:58:36 -0300

    aa

--- src/main/java/com/unisales/microservicoproduto/WebConfig.java
+++ src/main/java/com/unisales/microservicoproduto/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://angular-microservicos.vercel.app/") 
+                        .allowedOrigins("https://angular-microservicos.vercel.app/produtos") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- src/main/java/com/unisales/microservicoproduto/controllers/ProdutoController.java
+++ src/main/java/com/unisales/microservicoproduto/controllers/ProdutoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +20,6 @@ import com.unisales.microservicoproduto.services.ProdutoService;
 
 import jakarta.validation.Valid;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
